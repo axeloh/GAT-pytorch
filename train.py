e77fa475b739e66ddad73b29cb1171c7617654cb
axeloh <axel.harstad@gmail.com>
2020-12-24 01:17:08 +0100

    multi-head attention

--- train.py
+++ train.py
@@ -1,16 +1,13 @@
 import torch
-import torch.nn as nn
 import torch.nn.functional as F
 from torch_geometric.datasets import Planetoid
-import networkx as nx
 import matplotlib.pyplot as plt
-import numpy as np
 import time
 import os
-import sys
-import scipy.sparse as sp
-from torch.autograd import Variable
+import numpy as np
 
+
+from utils import print_info_about_dataset, plot_dataset, accuracy, create_adjacency_matrix
 from models import GAT
 
 
@@ -20,27 +17,38 @@ def train(model, optimizer, data, A, n_epochs, plot=False, device=None):
 	x = data.x.to(device)
 	targets = data.y.to(device)
 
-	train_mask = data.train_mask.to(device)
+	train_mask = torch.LongTensor(np.arange(140)).to(device)
+	val_mask = torch.LongTensor(np.arange(200, 500)).to(device)
+	#train_mask = data.train_mask.to(device)
+	#val_mask = data.val_mask.to(device)
 	start = time.time()
 
 	for epoch in range(n_epochs):
 		model.train()
 		optimizer.zero_grad()
-
 		out = model(x, A)
-		loss = F.cross_entropy(out[train_mask], targets[train_mask])
-		loss.backward()
+		train_loss = F.cross_entropy(out[train_mask], targets[train_mask])
+		train_acc = accuracy(out[train_mask], targets[train_mask])
+		train_loss.backward()
 		optimizer.step()
 
-		train_acc, _ = get_acc_and_loss(x, targets, model, A, device=device)
-		val_acc, val_loss = get_acc_and_loss(x, targets, model, A, type='val', device=device)
+		# Evaluate on validation set
+		model.eval()
+		out = model(x, A)
+		val_loss = F.cross_entropy(out[val_mask], targets[val_mask])
+		val_acc = accuracy(out[val_mask], targets[val_mask])
 
-		train_accuracies.append(train_acc)
-		train_losses.append(loss.item())
-		val_accuracies.append(val_acc)
-		val_losses.append(val_loss)
+		train_accuracies.append(train_acc.item())
+		train_losses.append(train_loss.item())
+		val_accuracies.append(val_acc.item())
+		val_losses.append(val_loss.item())
 
-		print(f'Epoch: {epoch}, Loss: {loss.item():.3f}, Train acc: {train_acc:.3f}, Val acc: {val_acc:.3f}', )
+		print(f'Epoch: {epoch+1}, '
+			  f'train_loss: {train_loss.item():.3f}, '
+			  f'train_acc: {train_acc.item():.3f}, '
+			  f'val_loss: {val_loss.item():.3f} '
+			  f'val_acc: {val_acc.item():.3f} ',
+			  )
 
 	print(f'Training done in {(time.time() - start):.1f}s')
 
@@ -65,94 +73,6 @@ def train(model, optimizer, data, A, n_epochs, plot=False, device=None):
 		plt.savefig('./outputs/att_accuracy_plot.png')
 		plt.show()
 		plt.close()
-
-
-def create_adjacency_matrix(num_nodes, edge_index, add_self_loops=True, normalize=True, device=None):
-	"""Creates adjacency matrix from pytorch_geometric edge_index"""
-	adj = torch.zeros((num_nodes, num_nodes))
-	edges = torch.stack((edge_index[0], edge_index[1]), 1)
-	for (source_i, target_i) in edges:
-		adj[source_i, target_i] = 1
-
-	if add_self_loops:
-		adj = adj + torch.eye(adj.size(0))
-
-	if normalize:
-		d = adj.sum(1)
-		adj = adj / d.view(-1, 1)
-
-	return adj.to(device)
-
-
-def get_acc_and_loss(x, targets, model, A, type='train', device=None):
-	model.eval()
-
-	correct = 0
-	out = model(x, A)
-	pred = out.max(dim=1)[1]
-
-	if type == 'train':
-		mask = data.train_mask.to(device)
-	elif type == 'val':
-		mask = data.val_mask.to(device)
-	else:
-		mask = data.test_mask.to(device)
-
-	loss = F.cross_entropy(out[mask], targets[mask]).item()
-	correct += pred[mask].eq(targets[mask]).sum().item()
-	acc = correct / (len(targets[mask]))
-
-	return acc, loss
-
-
-def print_info_about_dataset(dataset):
-	""" Expects dataset from torch_geometric.datasets"""
-	try:
-		data = dataset.data
-		print('------ PRINTING INFO ------')
-		print('>> INFO')
-		print(f'\tLength of dataset (number of graphs): {len(dataset)}')
-		if len(dataset) == 1:
-			print(f'\tData is one big graph.')
-		else:
-			print(f'\tData contains multiple graphs.')
-
-		print(f'\tNum nodes: {data.num_nodes}')
-		print(f'\tNum node features: {dataset.num_node_features}')
-		print(f'\tNum classes (node or graph classes): {dataset.num_classes}')
-		print(f'\tEdges contains attributes: {"False" if data.edge_attr is None else "True"}')
-		print(f'\tTarget (y) shape: {data.y.shape}')
-
-		if len(dataset) == 1:
-			print(f'\tNode feature matrix shape: {data.x.shape}')
-			print(f'\tContains isolated nodes: {data.contains_isolated_nodes()}')
-			print(f'\tContains self-loops: {data.contains_self_loops()}')
-			print(f'\tEdge_index shape: {data.edge_index.shape}')
-			print(f'\tEdges are directed: {data.is_directed()}')
-		else:
-			print(f'\tPrinting info about first graph:')
-			print(f'\t{dataset[0]}')
-		print('>> END')
-
-	except:
-		print('Some prints failed.')
-
-
-def plot_dataset(dataset):
-	edges_raw = dataset.data.edge_index.numpy()
-	edges = [(x, y) for x, y in zip(edges_raw[0, :], edges_raw[1, :])]
-	labels = dataset.data.y.numpy()
-
-	G = nx.Graph()
-	G.add_nodes_from(list(range(np.max(edges_raw))))
-	G.add_edges_from(edges)
-	plt.subplot(111)
-	options = {
-		'node_size': 30,
-		'width': 0.2,
-	}
-	nx.draw(G, with_labels=False, node_color=labels.tolist(), cmap=plt.cm.tab10, font_weight='bold', **options)
-	plt.show()
 
 
 if __name__ == '__main__':
@@ -190,5 +110,12 @@ if __name__ == '__main__':
 
 	train(model, optimizer, data, A, n_epochs=100, plot=True, device=device)
 
-	test_acc, _ = get_acc_and_loss(x, y, model, A, type='test', device=device)
-	print(f'---- Accuracy on test set: {test_acc}')
+	# Evaluate on test set
+	# test_mask = data.test_mask.to(device)
+	test_mask = torch.LongTensor(np.arange(500, 1500)).to(device)
+	model.eval()
+	out = model(x, A)
+	test_loss = F.cross_entropy(out[test_mask], y[test_mask])
+	test_acc = accuracy(out[test_mask], y[test_mask])
+
+	print(f'---- Accuracy on test set: {test_acc.item()}')
---
+++ utils.py
@@ -0,0 +1,79 @@
+import torch
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def create_adjacency_matrix(num_nodes, edge_index, add_self_loops=True, normalize=True, device=None):
+	"""Creates adjacency matrix from pytorch_geometric edge_index"""
+	adj = torch.zeros((num_nodes, num_nodes))
+	edges = torch.stack((edge_index[0], edge_index[1]), 1)
+	for (source_i, target_i) in edges:
+		adj[source_i, target_i] = 1
+
+	if add_self_loops:
+		adj = adj + torch.eye(adj.size(0))
+
+	if normalize:
+		d = adj.sum(1)
+		adj = adj / d.view(-1, 1)
+
+	return adj.to(device)
+
+
+def print_info_about_dataset(dataset):
+	""" Expects dataset from torch_geometric.datasets"""
+	try:
+		data = dataset.data
+		print('------ PRINTING INFO ------')
+		print('>> INFO')
+		print(f'\tLength of dataset (number of graphs): {len(dataset)}')
+		if len(dataset) == 1:
+			print(f'\tData is one big graph.')
+		else:
+			print(f'\tData contains multiple graphs.')
+
+		print(f'\tNum nodes: {data.num_nodes}')
+		print(f'\tNum node features: {dataset.num_node_features}')
+		print(f'\tNum classes (node or graph classes): {dataset.num_classes}')
+		print(f'\tEdges contains attributes: {"False" if data.edge_attr is None else "True"}')
+		print(f'\tTarget (y) shape: {data.y.shape}')
+
+		if len(dataset) == 1:
+			print(f'\tNode feature matrix shape: {data.x.shape}')
+			print(f'\tContains isolated nodes: {data.contains_isolated_nodes()}')
+			print(f'\tContains self-loops: {data.contains_self_loops()}')
+			print(f'\tEdge_index shape: {data.edge_index.shape}')
+			print(f'\tEdges are directed: {data.is_directed()}')
+		else:
+			print(f'\tPrinting info about first graph:')
+			print(f'\t{dataset[0]}')
+		print('>> END')
+
+	except:
+		print('Some prints failed.')
+
+
+def plot_dataset(dataset):
+	edges_raw = dataset.data.edge_index.numpy()
+	edges = [(x, y) for x, y in zip(edges_raw[0, :], edges_raw[1, :])]
+	labels = dataset.data.y.numpy()
+
+	G = nx.Graph()
+	G.add_nodes_from(list(range(np.max(edges_raw))))
+	G.add_edges_from(edges)
+	plt.subplot(111)
+	options = {
+		'node_size': 30,
+		'width': 0.2,
+	}
+	nx.draw(G, with_labels=False, node_color=labels.tolist(), cmap=plt.cm.tab10, font_weight='bold', **options)
+	plt.show()
+
+
+def accuracy(output, labels):
+    preds = output.max(1)[1].type_as(labels)
+    correct = preds.eq(labels).double()
+    correct = correct.sum()
+    return correct / len(labels)
+
